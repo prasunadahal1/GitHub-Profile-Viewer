e590ae760ad700475e948fb5250cf1d8b68422ef
Prasuna Dahal <prasunadahal909@gmail.com>
2026-07-09 17:08:03 +0545

    PR-05:working on user profile screen

--- lib/Providers/user_profile_provider.dart
+++ lib/Providers/user_profile_provider.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
+
 class UserProfileProvider extends ChangeNotifier{
   List<Map<String,dynamic>> _followersList=[];
   List <Map<String,dynamic>> get followersList=> _followersList;
@@ -16,6 +17,37 @@ class UserProfileProvider extends ChangeNotifier{
   String? _accessToken=dotenv.env['GITHUB_TOKEN'];
   String? get accessToken=>_accessToken;
 
+  Color getLanguageColor(String? color){
+    switch(color){
+      case "Dart":
+        return Color(0xff00B4AB);
+        
+      case "Java":
+        return Color(0xffB07219);
+
+      case "JavaScript":
+        return Color(0xffF1E05A);
+        
+      case "TypeScript":
+        return Color(0xff3178C6);
+        
+      case "Python":
+        return Color(0xff3572A5);
+
+      case "C++":
+        return Color(0xffF34B7D);
+
+      case "HTML":
+        return Color(0xffE34C26);
+
+      case"CSS":
+        return Color(0xff563D7C);
+
+        default:return
+          Colors.black;
+    }
+  }
+  
   Future<void> getFollowers( String keyword) async{
     try{
       Response response=await Dio().get("https://api.github.com/users/$keyword/followers",
@@ -80,4 +112,5 @@ class UserProfileProvider extends ChangeNotifier{
     }
     notifyListeners();
   }
+
 }
